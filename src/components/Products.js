e92af24fe7ab8b61310c12d740d09a3bc584123a
Hakim BAH <hakimbah@gmail.com>
2021-06-06 11:22:07 +0200

    update view

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,9 +14,9 @@ export default class Products extends Component {
                                     <p>{product.title}</p>
                                 </a>
                                 <div className="product-price">
-                                {formatCurrency(product.price)}
-                                </div>
+                                <div>{formatCurrency(product.price)}</div>
                                 <button className="button primary">Add to Cart</button>
+                            </div>
                             </div>
                         </li>
                     ))}
